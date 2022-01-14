75b926f9dedb716c66d5b008ee0995ca27039ef4
Abdalrhman Roston <80917742+AbdalrhmnRoston@users.noreply.github.com>
2022-01-14 21:13:15 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,41 @@
 
 
+
+// Header //
+let header = document.querySelector('header');
+let linksSection = document.querySelectorAll('header ul li');
+let openHeader = document.getElementById('open-header');
+
+openHeader.addEventListener('click', () => {
+    header.classList.toggle('header-open');
+})
+
+linksSection.forEach((el) => {
+    el.addEventListener('click', () => {
+        header.classList.remove('header-open');
+    })
+})
+
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > window.innerHeight) {
+        header.classList.add('header-fixed');
+    } else {
+        header.classList.remove('header-fixed');
+    }
+})
+
+// Open Header Automatic
+setTimeout(openHeaderAuto, 1500);
+
+function openHeaderAuto () {
+    header.classList.add('header-open');
+    function closeHeaderAuto () {
+    header.classList.remove('header-open');
+    }
+    // Close Header Automatic
+    setTimeout(closeHeaderAuto, 1300)
+}
+
 // Selected Item //
 let items = document.querySelectorAll('.item');
 
